ec5b4fb8f177853f49357401bc2c72b7973c0306
Chris Harcourt <chris.harcourt@gmail.com>
2021-08-30 21:15:11 +0100

    Add exoskeleton mk2

--- data.lua
+++ data.lua
@@ -1,3 +1,4 @@
 require("prototypes.battery-mk3-equipment")
+require("prototypes.exoskeleton-mk2-equipment")
 require("prototypes.fusion-reactor-mk3-equipment")
 require("prototypes.personal-roboport-mk3-equipment")
