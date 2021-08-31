f508a93f5ea71e57bf2c7be159fddbb0e43ec83c
Chris Harcourt <chris.harcourt@gmail.com>
2021-08-31 16:10:02 +0100

    Fix typo in energy shield filename

--- data.lua
+++ data.lua
@@ -1,5 +1,5 @@
 require("prototypes.battery-mk3-equipment")
-require("prototypes.energy-sheild-mk3-equipment")
+require("prototypes.energy-shield-mk3-equipment")
 require("prototypes.exoskeleton-mk2-equipment")
 require("prototypes.fusion-reactor-mk3-equipment")
 require("prototypes.personal-roboport-mk3-equipment")
