96d496c2328713e060fdaeb4307187ad7c378d01
Chris Harcourt <chris.harcourt@gmail.com>
2021-11-20 02:18:23 +0000

    Fix research prerequisites

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -6,7 +6,7 @@ data:extend{
     icons = util.technology_icon_constant_equipment(mod_gfx "technology/battery-mk3-equipment.png"),
 	  icon_size = 128,
     icon_mipmaps = 4,
-    prerequisites = {"battery-mk2-equipment", "utility-science-pack", "effectivity-module"},
+    prerequisites = {"battery-mk2-equipment", "space-science-pack", "effectivity-module"},
     effects =
     {
       {
@@ -35,7 +35,7 @@ data:extend{
     icons = util.technology_icon_constant_equipment(mod_gfx "technology/energy-shield-mk3-equipment.png"),
 	  icon_size = 128,
     icon_mipmaps = 4,
-    prerequisites = {"energy-shield-mk2-equipment", "military-4", "effectivity-module"},
+    prerequisites = {"energy-shield-mk2-equipment", "military-4", "effectivity-module", "space-science-pack"},
     effects =
     {
       {
@@ -65,7 +65,7 @@ data:extend{
     localised_description = {"technology-description.exoskeleton-equipment"},
     icons = util.technology_icon_constant_equipment(mod_gfx "technology/exoskeleton-mk2-equipment.png"),
 	  icon_size = 256, icon_mipmaps = 4,
-    prerequisites = {"exoskeleton-equipment", "production-science-pack", "utility-science-pack", "speed-module"},
+    prerequisites = {"exoskeleton-equipment", "space-science-pack", "speed-module"},
     effects =
     {
       {
@@ -95,7 +95,7 @@ data:extend{
     localised_description = {"technology-description.fusion-reactor-equipment"},
     icons = util.technology_icon_constant_equipment(mod_gfx "technology/fusion-reactor-mk2-equipment.png"),
 	  icon_size = 256, icon_mipmaps = 4,
-    prerequisites = {"fusion-reactor-equipment", "production-science-pack", "effectivity-module-3"},
+    prerequisites = {"fusion-reactor-equipment", "space-science-pack", "effectivity-module-3"},
     effects =
     {
       {
@@ -126,7 +126,7 @@ data:extend{
     icons = util.technology_icon_constant_equipment(mod_gfx "technology/personal-roboport-mk3-equipment.png"),
     icon_size = 256,
     icon_mipmaps = 4,
-    prerequisites = {"personal-roboport-mk2-equipment", "production-science-pack", "effectivity-module"},
+    prerequisites = {"personal-roboport-mk2-equipment", "space-science-pack", "effectivity-module"},
     effects =
     {
       {
@@ -155,7 +155,7 @@ data:extend{
     name = "exteq_power-armor-mk3",
     icon = mod_gfx "technology/power-armor-mk3.png",
     icon_size = 256, icon_mipmaps = 4,
-    prerequisites = {"power-armor-mk2", "speed-module-3", "effectivity-module-3"},
+    prerequisites = {"power-armor-mk2", "speed-module-3", "space-science-pack", "effectivity-module-3"},
     effects =
     {
       {
